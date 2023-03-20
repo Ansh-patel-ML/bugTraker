1eac33c98ecc20fc2754e52846d2df2b890817c8
Ansh-patel-ML <patelansh883@gmail.com>
2023-03-20 10:02:05 +0530

    removed raw code

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,52 +1,6 @@
-import { createAction, createReducer, createSlice } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 import { createSelector } from "@reduxjs/toolkit";
-// const BUG_ADD = "bugAddded";
-// const BUG_REMOVE = "bugRemoved";
-// const BUG_RESOLVED = "bugResolved";
-
-// export function bugAdded(description) {
-//   return {
-//     type: BUG_ADD,
-//     payload: {
-//       description,
-//     },
-//   };
-// }
-
-// export function bugResolved(id) {
-//   return {
-//     type: BUG_RESOLVED,
-//     payload: {
-//       id,
-//     },
-//   };
-// }
-
-// export const bugAdded = createAction("bugAdded");
-// export const bugResolved = createAction("bugResolved");
-// export const bugRemoved = createAction("bugRemoved");
-
 let lastId = 0;
-// let initialState = [];
-
-// export default createReducer([], {
-//   // action:function (event:eventHandler)
-//   [bugAdded.type]: (state, action) => {
-//     state.push({
-//       id: ++lastId,
-//       description: action.payload.description,
-//       resolve: false,
-//     });
-//   },
-//   [bugResolved.type]: (state, action) => {
-//     const bugIndex = state.findIndex((state) => state.id === action.payload.id);
-//     state[bugIndex].resolve = true;
-//   },
-//   [bugRemoved.type]: (state, action) => {
-//     const bugIndex = state.findIndex((state) => state.id === action.payload.id);
-//     state.splice(bugIndex, 1);
-//   },
-// });
 
 const bugSlice = createSlice({
   name: "Bugs",
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-// import reducer from "./bugs";
 import reducer from "./reducer";
-import { devToolsEnhancer } from "redux-devtools-extension";
 
 const store = configureStore({
   reducer: reducer,
